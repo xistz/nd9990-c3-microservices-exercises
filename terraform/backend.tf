e6d4b406bd88bd2c52a47d07e15cc6b6b697137b
Chan Jonathan <tmxistz@gmail.com>
2021-01-16 04:45:30 +0000

    used terraform cloud backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,4 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "xistz-dev"
+
+    workspaces {
+      name = "udagram-microservices"
+    }
+  }
 }
