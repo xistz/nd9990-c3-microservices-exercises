b18133dd5486723f269227c177987b7c368b3bc4
Chan Jonathan <tmxistz@gmail.com>
2021-01-16 06:30:20 +0000

    updated config

--- project/udagram-api/src/config/config.ts
+++ project/udagram-api/src/config/config.ts
@@ -1,14 +1,15 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  awsRegion: process.env.AWS_REGION,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsSecretAccessKey: process.env.AWS_SCRET_ACCESS_KEY,
+  awsMediaBucket: process.env.AWS_BUCKET,
+  url: process.env.URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
   },
 };
