8182fac6a5d7d88e06bd44d3f3016a099be634dd
Chan Jonathan <tmxistz@gmail.com>
2021-01-18 15:12:08 +0000

    added route table and associations

--- terraform/network.tf
+++ terraform/network.tf
@@ -7,46 +7,19 @@ resource "aws_vpc" "udagram" {
   }
 }
 
-resource "aws_subnet" "udagram_1" {
-  vpc_id            = aws_vpc.udagram.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-northeast-1a"
-
-  tags = {
-    Name = "udagram-subnet-1"
-  }
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
-resource "aws_subnet" "udagram_2" {
+resource "aws_subnet" "udagram_public" {
+  count = length(data.aws_availability_zones.available.names)
+
   vpc_id            = aws_vpc.udagram.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-northeast-1d"
+  cidr_block        = cidrsubnet(aws_vpc.udagram.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name = "udagram-subnet-2"
-  }
-}
-
-resource "aws_security_group" "udagram" {
-  name   = "udagram-security-group"
-  vpc_id = aws_vpc.udagram.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "udagram_dev"
+    Name = "udagram-public-subnet-${count.index}"
   }
 }
 
@@ -56,4 +29,29 @@ resource "aws_internet_gateway" "udagram" {
   tags = {
     Name = "udagram-internet-gateway"
   }
+}
+
+resource "aws_route_table" "udagram_public" {
+  vpc_id = aws_vpc.udagram.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.udagram.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.udagram.id
+  }
+
+  tags = {
+    Name = "udagram-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "udagram_public" {
+  count = length(aws_subnet.udagram_public)
+
+  subnet_id      = aws_subnet.udagram_public[count.index].id
+  route_table_id = aws_route_table.udagram_public.id
 }
