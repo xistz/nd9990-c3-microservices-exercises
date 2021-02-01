030e391257308cab00e628a36f63dcbba813ed9e
Chan Jonathan <tmxistz@gmail.com>
2021-02-01 23:44:02 +0900

    removed feed source files from user-api

--- project/udagram-user-api/src/config/config.ts
+++ project/udagram-user-api/src/config/config.ts
@@ -4,10 +4,6 @@ export const config = {
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   dialect: 'postgres',
-  awsRegion: process.env.AWS_REGION,
-  awsAccessKeyId: process.env.AWS_BUCKET_ACCESS_KEY_ID,
-  awsSecretAccessKey: process.env.AWS_BUCKET_SECRET_ACCESS_KEY,
-  awsMediaBucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
