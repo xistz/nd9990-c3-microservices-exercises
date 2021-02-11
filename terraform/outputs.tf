c1fb115cb31f4dcd864d0bba9c1b36f84d0026ed
Chan Jonathan <tmxistz@gmail.com>
2021-02-11 23:33:22 +0900

    use local file to generate k8s secrets

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,27 @@
-output "db_host" {
-  value = aws_db_instance.udagram.address
+resource "random_password" "secret" {
+  length = 16
+}
+
+locals {
+  k8s_secrets = <<-K8SSECRETS
+  ---
+  apiVersion: v1
+  kind: Secret
+  metadata:
+    name: udagram-secrets
+  type: Opaque
+  stringData:
+    db_host: ${aws_db_instance.udagram.address}
+    db_user: ${aws_db_instance.udagram.username}
+    db_password: ${aws_db_instance.udagram.password}
+    aws_bucket: ${aws_s3_bucket.udagram.bucket}
+    aws_bucket_access_key_id: ${aws_iam_access_key.udagram_bucket.id}
+    aws_bucket_secret_access_key: ${aws_iam_access_key.udagram_bucket.secret}
+    jwt_secret: ${random_password.secret.result}
+  K8SSECRETS
+}
+
+resource "local_file" "k8s_secrets" {
+  content  = local.k8s_secrets
+  filename = "${path.module}/../project/k8s/udagram-secrets.yaml"
 }
