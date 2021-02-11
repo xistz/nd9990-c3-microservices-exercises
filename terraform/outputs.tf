0cd4c8f284549407fe8db61c4487b13de25dd26e
Chan Jonathan <tmxistz@gmail.com>
2021-02-12 00:06:34 +0900

    added local file to generate .env.dev for docker-compose

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -19,9 +19,25 @@ locals {
     aws_bucket_secret_access_key: ${aws_iam_access_key.udagram_bucket.secret}
     jwt_secret: ${random_password.secret.result}
   K8SSECRETS
+  env_dev     = <<-ENVDEV
+  POSTGRES_USERNAME=udagram
+  POSTGRES_PASSWORD=${aws_db_instance.udagram.password}
+  POSTGRES_DB=udagram_dev
+  POSTGRES_HOST=${aws_db_instance.udagram.address}
+  AWS_REGION=ap-northeast-1
+  AWS_BUCKET=${aws_s3_bucket.udagram.bucket}
+  AWS_BUCKET_ACCESS_KEY_ID=${aws_iam_access_key.udagram_bucket.id}
+  AWS_BUCKET_SECRET_ACCESS_KEY=${aws_iam_access_key.udagram_bucket.secret}
+  JWT_SECRET=${random_password.secret.result}
+  ENVDEV
 }
 
 resource "local_file" "k8s_secrets" {
-  content  = local.k8s_secrets
-  filename = "${path.module}/../project/k8s/udagram-secrets.yaml"
+  sensitive_content = local.k8s_secrets
+  filename          = "${path.module}/../project/k8s/udagram-secrets.yaml"
+}
+
+resource "local_file" "env_dev" {
+  sensitive_content = local.env_dev
+  filename          = "${path.module}/../project/.env.dev"
 }
