4eba3aacd8565a8ff8148296aafa441c366e3abe
Chan Jonathan <tmxistz@gmail.com>
2021-02-20 00:16:54 +0900

    moved kubernetes provider to providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,35 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "cloud-developer-terraform"
+}
 
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "2.0.0"
-    }
-
-    null = {
-      source  = "hashicorp/null"
-      version = "3.0.0"
-    }
-
-    template = {
-      source  = "hashicorp/template"
-      version = "2.2.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
-    }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
   }
-
-  required_version = "~> 0.14"
 }
