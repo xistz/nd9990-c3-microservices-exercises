3e171e0430e2879a05bb1d67940321f85a54eba0
Chan Jonathan <tmxistz@gmail.com>
2021-02-20 03:41:25 +0900

    added variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  type    = string
+  default = "ap-northeast-1"
+}
+
 variable "bucket_name" {
   type    = string
   default = "xistz-udagram-microservices-dev"
@@ -8,7 +13,17 @@ variable "db_identifier" {
   default = "udagram-mircoservices-dev"
 }
 
+
+variable "db_username" {
+  type    = string
+  default = "udagram"
+}
+
 variable "db_name" {
   type    = string
   default = "udagram_dev"
+}
+
+variable "cluster_name" {
+  default = "udagram-eks"
 }
