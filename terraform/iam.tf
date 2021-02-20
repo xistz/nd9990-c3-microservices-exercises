5dc3d9e9ee36a1c7510947325784c95493ba6abd
Chan Jonathan <tmxistz@gmail.com>
2021-02-20 16:54:59 +0900

    use iam-user module to create iam users and access keys

---
+++ terraform/iam.tf
@@ -0,0 +1,8 @@
+module "iam_users" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  for_each = toset(["udagram-bucket", "udagram-kubectl"])
+
+  create_iam_user_login_profile = false
+  name                          = each.value
+}
